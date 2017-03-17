14b86a5cb6df9264db457f472faa0330b2bee8a7
alshatub <ahmed.alshatub@yahoo.com>
2017-03-18 00:06:57 +0300

    Update trengale.cpp

--- trengale.cpp
+++ trengale.cpp
@@ -4,19 +4,18 @@ int main(){
     int row_co,col_co,n;
     cout<<"please enter the num of last rows\n";
    cin>>n;
-    for(row_co=0;row_co<=n;row_co++){
-        for (col_co=0; col_co<=row_co; col_co++) {
-cout<<col_co*row_co<<"\t";
+    int arr[n][n];
+    for(row_co=0;row_co<n;row_co++){
+        for (col_co=0; col_co<n; col_co++) {
+           cin>>arr[row_co][col_co];
         }
-cout<<endl;
-
-
-
-
-
-
-
-
+    }
+    cout<<"\n\n\n\n\n\n";
+    for(row_co=0;row_co<n;row_co++){
+        for (col_co=0; col_co<=row_co; col_co++) {
+            cout<<arr[row_co][col_co]<<"\t";
+        }
+        cout<<endl;
     }
 
 
